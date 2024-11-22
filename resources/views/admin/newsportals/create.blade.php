91310853bf2cb48a3722fb2b2a834c9652ccc310
vivin <vivinetan07@gmail.com>
2024-11-22 13:32:26 +0700

    test vue.js 11/22/2024

--- resources/views/admin/newsportals/create.blade.php
+++ resources/views/admin/newsportals/create.blade.php
@@ -56,10 +56,25 @@
                                 </textarea>
                             </div>
                         </div>
-                        <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <div class="flex space-x-2">
+                            <button
+                            @click="increment"
+                                type="button" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                                Increment
+                            </button>
+                            <p>@{{ number }}</p>
+                            <button
+                            @click="decrement"
+                                type="button" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                                Decrement
+                            </button>
+                        </div>
+                        {{-- <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                             Add new product
-                        </button>
+                        </button> --}}
                     </form>
 
                 </div>
@@ -75,8 +90,17 @@
             const message = ref('Hello Vue!')
             const x = ref('')
 
-            watch(x, (newX) => {
-            console.log(`x is ${newX}`)
+            //Mendeklarasikan Variable
+            const number = ref(0);
+            const increment = () => {
+                number.value++;
+            }
+            const decrement = () => {
+                number.value--;
+            }
+
+            watch(number, (newX) => {
+            console.log(`number is ${newX}`)
             })
 
             //Deklarasi objek items
@@ -88,6 +112,9 @@
               message,
               items,
               x,
+              number,
+              increment,
+              decrement,
             }
           }
         }).mount('#vue-app')
